7494867734169fbce6e8c131a314041acb4a8cbe
Gustavo A Salazar Gomez <gustavo.salazar@uao.edu.co>
2021-12-28 11:35:45 -0500

    update environments

--- highway_env/__init__.py
+++ highway_env/__init__.py
@@ -1,5 +1,11 @@
-# Hide pygame support prompt
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-# Import the envs module so that envs register themselves
-import highway_env.envs
+from highway_env.envs.highway_env import *
+from highway_env.envs.merge_env import *
+from highway_env.envs.parking_env import *
+from highway_env.envs.summon_env import *
+from highway_env.envs.roundabout_env import *
+from highway_env.envs.two_way_env import *
+from highway_env.envs.intersection_env import *
+from highway_env.envs.lane_keeping_env import *
+from highway_env.envs.u_turn_env import *
+from highway_env.envs.exit_env import *
+from highway_env.envs.racetrack_env import *
