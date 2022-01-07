33214f0712fc986ef5d42c110999ea5b294bbcbb
gsg213 <gsg213@github.com>
2022-01-07 21:23:29 +0100

    Added subplot to display vehicle states

--- ReinforcementLearning.py
+++ ReinforcementLearning.py
@@ -5,6 +5,7 @@ Created on Sun Dec 19 15:51:48 2021
 
 @author: anamaria
 """
+from json.encoder import py_encode_basestring_ascii
 import gym
 import highway_env
 from stable_baselines3 import DQN
@@ -32,10 +33,20 @@ env = gym.make("roundabout-v130")
 
 
 actions = []
+
+
+vel = []
+target_vel=[]
+acc=[]
+pos=[]
+lat_vel=[]
+
+action_names = ['LEFT LANE','IDLE','RIGHT LANE','FASTER','SLOWER']
 df = pd.DataFrame(columns = ['vehicle', 'x','y','vx','vy'])
 # Load and test saved model
 model = DQN.load("highway_dqn/model")
 done = False
+t = 0
 while done == False:
   done = False
   obs = env.reset()
@@ -46,29 +57,62 @@ while done == False:
     env.render()
     dft = pd.DataFrame(obs,columns = ['vehicle', 'x','y','vx','vy'])
     df = df.append(dft)
-    print(obs)
-    #env.viewer.set_agent_action_sequence(action)
+    
+    v, tv, ac, p, lv = env.vehicleInfo()
+    vel.append(v)
+    target_vel.append(tv)
+    acc.append(ac)
+    pos.append([p[0],p[1]])
+    lat_vel.append(lv)
+
+    t+=1
+        
 
 step = np.arange(len(actions)) 
-# plt.step(step,actions)
+pos_ = np.array([np.array(xi) for xi in pos])
 
 df_ego = df.loc[0]
 df_ego['vel'] = ((df_ego['vx'])**2 + (df_ego['vy'])**2 )**(1/2) * 15
-vel = df_ego['vel'].to_numpy()
-# plt.plot(step,vel)
+vel_ = df_ego['vel'].to_numpy()
 
+fig = plt.figure(figsize=(15, 11))
 
-fig, ax1 = plt.subplots()
+grid = plt.GridSpec(3, 2, wspace=0.4, hspace=0.3)
 
-ax2 = ax1.twinx()
-ax1.step(step, actions, 'g-')
-ax2.plot(step, vel, 'b-')
+plt.subplot(grid[0, 0])
+plt.title('Speed (m/s)')
+plt.plot(range(t),vel,label = "Speed")
+plt.plot(range(t), target_vel, label = "Target")
+plt.legend()
+plt.grid()
 
-ax1.set_xlabel('Step')
-ax1.set_ylabel('Action', color='g')
-ax1.set_ylim(0,5)
-ax1.set_ylim(0,20)
-ax2.set_ylabel('Speed (m/s)', color='b')
+# plt.subplot(3, 2, 2)
+# plt.title('Steering angle (rad)')
+# plt.plot(ran_t,heading, label = "Heading")
+# plt.plot(ran_t,steering_angle, label = "Target")
+# plt.legend()
+# plt.grid()
+
+plt.subplot(grid[1, 0])
+plt.title('Acceleration (m/s²)')
+plt.plot(range(t),acc)
+plt.grid()
+
+plt.subplot(grid[0, 1])
+plt.title('Actions')
+plt.yticks([0,1,2,3,4], action_names)
+plt.step(range(t),actions)
+plt.ylim(-1, 5) 
+plt.grid()
+
+x=pos_[:,0]
+y=pos_[:,1]
+
+plt.subplot(grid[1:, 1])
+plt.title('Position (x,y)  (m)')
+plt.plot(x,y)
+plt.ylim(max(y)*1.30, min(y)*1.30)
+plt.xlim(min(x)*1.30, max(x)*1.30)
+plt.grid()
 
 plt.show()
-   
